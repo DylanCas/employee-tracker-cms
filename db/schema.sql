5a74f11de52057a0c0d695a059b4f88f359045e4
DylanCas <djcasabona12@gmail.com>
2023-01-12 10:29:20 -0700

    corrected syntax for create table items

--- db/schema.sql
+++ db/schema.sql
@@ -12,18 +12,18 @@ CREATE TABLE department (
 
 
 -- TODO: Implement foreign-primary key biz
--- CREATE TABLE role {
---      id INT,
---      title VARCHAR(30),
---      salary DECIMAL,
---      department_id INT
--- }
+CREATE TABLE role (
+     id INT,
+     title VARCHAR(30),
+     salary DECIMAL,
+     department_id INT
+)
 
--- -- TODO: Implement foreign-primary key biz
--- CREATE TABLE employee {
---     id INT,
---     first_name VARCHAR(30),
---     last_name VARCHAR(30),
---     role_id INT,
---     manager_id INT
--- }
+-- TODO: Implement foreign-primary key biz
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+)
